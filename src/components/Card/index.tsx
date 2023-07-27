91e54ffa0298cde7c397d2d1353dcdf2f0ea4066
Ralf Drehmer Wink <105680174+ralfdw3@users.noreply.github.com>
2023-07-27 09:18:58 -0300

    some adjusts in home page but still unfinished

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,18 +3,44 @@ import { AiFillCloseCircle, AiFillEdit } from "react-icons/ai";
 import { ICard } from "../../common/ICard";
 
 const Card = () => {
+  const calcDiffDays = (date: string) => {
+    const today = new Date();
+    const dateCard = new Date(date);
+    console.log(dateCard.getDate() - today.getDate() + 1);
+    return dateCard.getDate() - today.getDate() + 1;
+  };
+
+  const apperrenceDate = "2023-07-26";
+
+  const diffInDays = calcDiffDays(apperrenceDate);
+
+  let gradient;
+  if (diffInDays <= -7) {
+    gradient = "red-gradient";
+  } else if (diffInDays >= 0) {
+    gradient = "green-gradient";
+  } else {
+    gradient = "yellow-gradient";
+  }
+
   return (
-    <div className="card-container">
-      <div className="card-icons">
-        <AiFillEdit size={25} className="card-icon" />
-        <AiFillCloseCircle size={25} className="card-icon" />
+    <div className={`card-container ${gradient}`}>
+      <div className="card-header">
+        <span className="card-apperrence-date">{apperrenceDate}</span>
+        <div className="card-icons">
+          <AiFillEdit size={25} className="card-icon" />
+          <AiFillCloseCircle size={25} className="card-icon" />
+        </div>
       </div>
       <span className="card-title">Titulo do card</span>
       <span className="card-company-name">Nome da empresa</span>
-      <textarea className="card-description" spellCheck="false">
-        descricao grande ou nao
-        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-      </textarea>
+      <textarea
+        className="card-description"
+        spellCheck="false"
+        defaultValue={`descricao grande ou nao aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+        `}
+        disabled={true}
+      />
     </div>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Header from "../../components/Header";
 import "./Home.css";
-import DateSelector from "../../components/DateSelector";
 import Card from "../../components/Card";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 const Home = () => {
   return (
     <section className="page-home">
       <Header />
-      <DateSelector />
+      <div className="create-new-card">
+        <span>Novo card</span>
+        <AiFillPlusCircle size={30} className="create-new-card-icon" />
+      </div>
+
       <div className="home-cards cards-container">
         <Card />
         <Card />
